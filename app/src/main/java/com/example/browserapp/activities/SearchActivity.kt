e2fee5973c4ac6f044a369be9d62f7b621f2044f
Muhammad Saleh <m.salehjz@gmail.com>
2024-01-04 14:12:51 +0500

    todo implement image and videos

---
+++ app/src/main/java/com/example/browserapp/activities/SearchActivity.kt
@@ -0,0 +1,47 @@
+package com.example.browserapp.activities
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import com.example.browserapp.R
+import com.example.browserapp.databinding.ActivitySearchBinding
+import com.example.browserapp.searchfragments.ImagesFragment
+import com.example.browserapp.searchfragments.VideosFragment
+import com.example.browserapp.searchfragments.WebPagesFragment
+
+class SearchActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySearchBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        try {
+            super.onCreate(savedInstanceState)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            binding = ActivitySearchBinding.inflate(layoutInflater)
+
+            setContentView(binding.root)
+            binding.btnWebPages.setOnClickListener {
+                replaceFragment(WebPagesFragment())
+            }
+            binding.btnImages.setOnClickListener {
+                replaceFragment(ImagesFragment())
+            }
+//
+            binding.btnVideos.setOnClickListener {
+                replaceFragment(VideosFragment())
+            }
+        }catch (e:Exception){
+            Log.e("TAGINN",e.stackTraceToString())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+
+    }
+}
+
+
